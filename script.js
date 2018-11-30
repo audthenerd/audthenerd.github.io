eb3c0bfb582805c58d45ce8a20366e3bb1c947db
audthenerd <aud.kow88@gmail.com>
2018-11-30 11:53:17 +0800

    added svg file and css animations

--- script.js
+++ script.js
@@ -14,14 +14,14 @@ $(document).scroll(function() {
 
 //***********************************************
 //***********************************************
-//FONT TYPING FOR CAPTION HEADER
+//FONT TYPING ANIMATION FOR CAPTION HEADER
 //***********************************************
 //***********************************************
  var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
+  this.period = parseInt(period, 10) || 500;
   this.txt = '';
   this.tick();
   this.isDeleting = false;
